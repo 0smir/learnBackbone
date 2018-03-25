d4478c32a7628598cd512c27787dbc6b1d924dcb
0smir <o.v.smirnova1987@gmail.com>
2018-03-26 00:35:35 +0300

    learn how to work with views, differents between model and views

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ var Advertisement = Backbone.Model.extend({
 
     getPriceInRUB: function(rate) {
         return this.get('price') * rate;
+    },
+    getPricePerKilometerTravelled: function () {
+       return (this.get('price')/this.get('odometer')).toFixed(2);
     }
 
 });
@@ -38,9 +41,12 @@ var ad = new Advertisement({
     make: 'Mazda',
     model: 'Atenza',
     year: 2007,
-    price: 1700000
+    price: 1700000,
+    odometer:70000
 
 });
+
+
 
 
 
@@ -48,19 +54,43 @@ console.log('ad price: ', ad.get('price'));
 console.log('ad make: ', ad.get('make'));
 console.log('ad year', ad.get('year'));
 
-ad.set('year', 2016, {silent: true});
+// ad.set('year', 2016, {silent: true});
 
-console.log('ad year after update: ', ad.get('year'));
+// console.log('ad year after update: ', ad.get('year'));
 
 
 var  attrAD = ad.toJSON();
 
 console.log('attrAD: ', attrAD);
-
-ad.set('price', -120000, {validate: true});
-console.log('ad price update: ', ad.get('price'));
-ad.set('year', 2017);
-console.log('ad year update again: ', ad.get('year'));
+//
+// ad.set('price', -120000, {validate: true});
+// console.log('ad price update: ', ad.get('price'));
+// ad.set('year', 2017);
+// console.log('ad year update again: ', ad.get('year'));
 
 var RUBJPY = 0.32;
 console.log('price in RUB: ', ad.getPriceInRUB(RUBJPY));
+
+
+
+var AdvertisementViev = Backbone.View.extend({
+    tagName: 'li',
+
+    template: _.template("Продам <%= make %> <%= model %> за <%= price %> иен"),
+
+    initialize: function () {
+      this.render();
+    },
+
+    render: function () {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    }
+});
+
+var advertismentView = new AdvertisementViev({ model: ad });
+// advertismentView.render();
+console.log('advertismentView: ', advertismentView.model.get('make'));
+
+console.log('advertismentView odometr', advertismentView.model.getPricePerKilometerTravelled());
+console.log('attention: ', advertismentView.el);
