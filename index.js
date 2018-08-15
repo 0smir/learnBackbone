425719c4086144551b855156636b7851953119c4
0smir <o.v.smirnova1987@gmail.com>
2018-08-16 00:04:31 +0300

    learn view, template, validation, render, initialize

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ var Advertisement = Backbone.Model.extend({
         price: 2000000
     },
 
+
     initialize: function () {
-        console.log('Create a new copy of Advertisement model!');
+        // console.log('Create a new copy of Advertisement model!');
 
         this.on('change:price', function () {
-            alert('New price was set!' + this.get('price'));
+            console.log('New price was set!' + this.get('price'));
         });
 
         this.on('change:year', function () {
-            alert('New year was set!' + this.get('year'));
+            console.log('New year was set!' + this.get('year'));
         });
         this.on('invalid', function (model, error) {
-            alert(error);
+            console.log(error);
         });
     },
 
@@ -32,9 +33,16 @@ var Advertisement = Backbone.Model.extend({
     },
     getPricePerKilometerTravelled: function () {
        return (this.get('price')/this.get('odometer')).toFixed(2);
-    }
+    },
+
+    setError: function (model, error) {
+        console.log(error);
+    },
 
 });
+
+
+
 
 var ad = new Advertisement({
 
@@ -42,55 +50,79 @@ var ad = new Advertisement({
     model: 'Atenza',
     year: 2007,
     price: 1700000,
-    odometer:70000
+    odometer:70000,
+
 
 });
 
-
-
-
-
-console.log('ad price: ', ad.get('price'));
-console.log('ad make: ', ad.get('make'));
-console.log('ad year', ad.get('year'));
-
-// ad.set('year', 2016, {silent: true});
-
-// console.log('ad year after update: ', ad.get('year'));
-
-
-var  attrAD = ad.toJSON();
-
-console.log('attrAD: ', attrAD);
-//
 // ad.set('price', -120000, {validate: true});
-// console.log('ad price update: ', ad.get('price'));
-// ad.set('year', 2017);
-// console.log('ad year update again: ', ad.get('year'));
-
-var RUBJPY = 0.32;
-console.log('price in RUB: ', ad.getPriceInRUB(RUBJPY));
 
 
+var Element = Backbone.View.extend({
 
-var AdvertisementViev = Backbone.View.extend({
+    parent: 'ol',
+
     tagName: 'li',
-
-    template: _.template("Продам <%= make %> <%= model %> за <%= price %> иен"),
-
+    className: 'list-item',
+    template: _.template($('#ad_template').html()),
+    
     initialize: function () {
-      this.render();
+        // this.listenTo(this.model, 'run', this.render);
+        // this.model.trigger('run');
+        this.render();
     },
 
     render: function () {
-        this.$el.html(this.template(this.model.toJSON()));
-        return this;
+        console.log('render');
+        this.$el.html( this.template(this.model.toJSON()));
     }
+
 });
 
-var advertismentView = new AdvertisementViev({ model: ad });
-// advertismentView.render();
-console.log('advertismentView: ', advertismentView.model.get('make'));
+var carItem = new Element({
+    model: ad,
+    initialize: function () {
+        console.log('model:', this.model);
+    },
+    render: function () {
 
-console.log('advertismentView odometr', advertismentView.model.getPricePerKilometerTravelled());
-console.log('attention: ', advertismentView.el);
+    }
+});
+console.log('carItem', carItem);
+
+
+// var  attrAD = ad.toJSON();
+//
+// console.log('attrAD: ', attrAD);
+// //
+// // ad.set('price', -120000, {validate: true});
+// // console.log('ad price update: ', ad.get('price'));
+// // ad.set('year', 2017);
+// // console.log('ad year update again: ', ad.get('year'));
+//
+// var RUBJPY = 0.32;
+// console.log('price in RUB: ', ad.getPriceInRUB(RUBJPY));
+//
+//
+//
+// var AdvertisementViev = Backbone.View.extend({
+//     tagName: 'li',
+//
+//     template: _.template("Продам <%= make %> <%= model %> за <%= price %> иен"),
+//
+//     initialize: function () {
+//       this.render();
+//     },
+//
+//     render: function () {
+//         this.$el.html(this.template(this.model.toJSON()));
+//         return this;
+//     }
+// });
+//
+// var advertismentView = new AdvertisementViev({ model: ad });
+// // advertismentView.render();
+// console.log('advertismentView: ', advertismentView.model.get('make'));
+//
+// console.log('advertismentView odometr', advertismentView.model.getPricePerKilometerTravelled());
+// console.log('attention: ', advertismentView.el);
